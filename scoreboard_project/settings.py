b59271600660d63985b1278e938fae73fe65fbe9
jabeel shahis <shahis@exthgen.in>
2025-11-27 17:10:05 +0530

    updated sqllite in settings

--- scoreboard_project/settings.py
+++ scoreboard_project/settings.py
@@ -53,12 +53,8 @@ WSGI_APPLICATION = 'scoreboard_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'score_board',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
